a895bb0365b685d75580c94ffd3c4a5a4356889b
chikini91 <khizhnyak.oleg91@gmail.com>
2018-02-23 16:17:20 +0200

    fix2

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
 
-    $(".leftLine").css("right","0");
-    $(".rigthLine").css("left","0");
+    $(".leftLine").animate({"right":"0"},1000);
+    $(".rigthLine").animate({left:"0"},1000);;
 
     var currentOffset = window.pageYOffset;
     var header = $("header");
@@ -13,16 +13,20 @@ $(document).ready(function() {
         // add fixed menu
 
         if (currentOffset > $(".menu").height()){
-            $(".menu").css("position","fixed")
+            $(".menu").css("position","fixed");
+            // header.height(header.height() + $(".menu").height())
+            $(".lines").css("margin-top", "70px" )
+
+
         };
 
             var diff = window.pageYOffset - currentOffset;
             currentOffset = window.pageYOffset;
             if (diff > 0) {
-                $(".menu").css("top","-70px");
+                $(".menu").css("top","-70px").css('transition-duration','1s');
             }
             else {
-                $(".menu").css("top","0");
+                $(".menu").css("top","0").css('transition-duration','1s');
 
             }
 
@@ -38,10 +42,10 @@ $(document).ready(function() {
     });
 
     function animateProduct() {
-        $(".description").css("opacity","1");
-        $(".mainImage").css("left","0");
-        $(".duplicateImage").css("left","0");
-        $('.whiteRectangleWrapper').css("margin-left","0")
+        $(".description").css("opacity","1").css('transition-duration','1s');
+        $(".mainImage").css("left","0").css('transition-duration','1s');
+        $(".duplicateImage").css("left","0").css('transition-duration','1s');
+        $('.whiteRectangleWrapper').css("margin-left","0").css('transition-duration','1s')
     }
 
     $(".item").click(function (e) {
