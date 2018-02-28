e398b6e3d40555433de8d084db13780f4233f5ce
chikini91 <khizhnyak.oleg91@gmail.com>
2018-02-28 14:32:17 +0200

    remake some html structure

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
 $(document).ready(function() {
 
-
-    $(".leftLine").animate({"right":"0"},1000);
-    $(".rigthLine").animate({left:"0"},1000);
+    $(".rightAnimateLine").animate({"left":"170px"},1000);
+    $(".leftAnimateLine").addClass("active");
 
     var currentOffset = window.pageYOffset;
     var header = $("header");
+    var menu = $(".menu");
     var world = $(".world").height();
 
     $(window).scroll(function() {
 
         // add fixed menu
 
-        if (currentOffset > $(".menu").height()){
-            $(".menu").css({"position":"fixed","box-shadow":"0px 0px 5px 2px gray"});
-            $(".lines").css("padding-top", "370px" )
-
-
+        if (currentOffset > menu.height()){
+            menu.css({"position":"fixed","box-shadow":"0px 0px 5px 2px gray"});
+            $(".lines").addClass('jspadding')
         };
 
             var diff = window.pageYOffset - currentOffset;
             currentOffset = window.pageYOffset;
 
              if(diff > 0) {
-                $(".menu").css("top","-70px").css('transition-duration','1s');
+                 menu.css({"top":"-70px","transition-duration":"1s"});
             }
+
             else {
-                $(".menu").css("top","0").css('transition-duration','1s');
-                 if (currentOffset === 0) {
-                     $(".menu").css("box-shadow","none").css('transition-duration','0s');
+                 menu.css({"top":"0","transition-duration":"1s","background":"url(./img/bg.png)"});
+
+                 if (currentOffset === 0 && window.pageYOffset ===0 ) {
+                     menu.css({"box-shadow":"none",
+                               "position":"static",
+                               "box-sizing":"border-box",
+                               "background":"transparent",
+                               "transition-duration":"0s"});
+                     $(".lines").removeClass("jspadding");
                  }
             }
 
@@ -46,7 +51,7 @@ $(document).ready(function() {
     function animateProduct() {
         $(".description").css("opacity","1").css('transition-duration','1s');
         $(".mainImage").css("left","0").css('transition-duration','1s');
-        $(".duplicateImage").css("left","0").css('transition-duration','1s');
+        $(".duplicateImage").css("left","35%").css('transition-duration','1s');
         $('.whiteRectangleWrapper').css("margin-left","0").css('transition-duration','1s')
     }
 
